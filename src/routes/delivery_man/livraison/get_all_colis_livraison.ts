211431c36ee21b85c74a3b6d8438083574b4e9a2
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-01-16 22:26:07 +0100

    Fini ?

---
+++ src/routes/delivery_man/livraison/get_all_colis_livraison.ts
@@ -0,0 +1,29 @@
+import { Application, Request, Response } from "express";
+import { Adress, Colis, attributesColis } from "../../../models";
+
+//get all the colis of the livraison you want
+
+export const deliveryManGetAllLivraisonColis = (app : Application)=>{
+  app.get('/deliveryman/colis/:livraison_id', async (req : Request, res : Response) => {
+    try{
+      const colis = await Colis.findAll({
+        where: {
+          [attributesColis.livraison_id] : req.params.livraison_id,
+        },
+        include:[
+          {
+            as: 'adress',
+            model: Adress,
+          }
+        ]
+      });
+      res.status(200).json(colis);
+      return;
+    } 
+    catch (err) {
+      console.log(err);
+      res.status(500).send({ message: "Internal server error" });
+      return;
+    }
+  });
+}
--- src/routes/delivery_man/livraison/index.ts
+++ src/routes/delivery_man/livraison/index.ts
@@ -1,2 +1,3 @@
 export * from './get_all_livraison';
 export * from './get_colis_of_livraison';
+export * from './get_all_colis_livraison';
