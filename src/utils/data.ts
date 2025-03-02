ae68de9d0f2ca72224bbbf15f82dbcee4d24543c
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-03-02 10:45:36 +0100

    Add of nodemailer

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,20 @@
+import nodemailer from 'nodemailer';
+
+
 export const keyToken = '1iAàLaN0Q-6B@&$B';
 
 export const passwordDb = 'root';
 export const userDb = 'root';
 export const hostDb = 'efd_db';
+
+export const googleEmail= "efd.backend@gmail.com";
+
+export const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: googleEmail,
+    pass: 'jfxk zngm tsoq rlmr',
+  },
+});
 
 
