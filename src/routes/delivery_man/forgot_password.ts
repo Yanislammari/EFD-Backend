87fd3471a0efc5e4074168e7278961bfc2eb03e3
Valentin <valentin@MacBook-Air-de-Valentin.local>
2025-03-02 10:49:03 +0100

    Can I ?

--- src/routes/delivery_man/forgot_password.ts
+++ src/routes/delivery_man/forgot_password.ts
@@ -5,7 +5,7 @@ import { emailEFD } from "../../utils";
 
 
 export const getNewPasswd = (app : Application)=>{
-  app.patch('/deliveryman/forgot_password', async (req : Request, res : Response) => {
+  app.patch('/deliveryman/forgot_password', async (req : Request, res : Response):Promise<any> => {
     try{
       const clearPasswd = Math.random().toString(36).substring(2,10);
       const salt = await bcrypt.genSalt(10);
@@ -28,6 +28,8 @@ export const getNewPasswd = (app : Application)=>{
         subject : "Forgotten password EFD",
         text : "You forgot your password here is the new one : \n\n"+clearPasswd
       }
+
+      tra
 
     }
     catch (err) {
